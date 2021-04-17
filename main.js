ff52a78f7586aaae08bd11fab1f1c58bdc3f5670
ayushigauri28 <40198747+ayushigauri28@users.noreply.github.com>
2021-04-18 01:14:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+function change(){
+    document.getElementById("what").innerHTML= "→";
+}
+function changet(){
+    document.getElementById("near").innerHTML= "→";
+}
+function changeth(){
+    document.getElementById("sh").innerHTML= "→";
+}
+function changer(){
+    document.getElementById("read").innerHTML= "→";
+}
+function changed(){
+    document.getElementById("download").innerHTML= "→";
+}
+function changek(){
+    document.getElementById("know").innerHTML= "→";
+}
+function changeback(){
+    document.getElementById("what").innerHTML= "WHAT'S NEW";
+}
+function changebackt(){
+    document.getElementById("near").innerHTML= "FIND A CAFE NEAR YOU";
+}
+function changebackth(){
+    document.getElementById("sh").innerHTML= "SHOP NOW";
+}
+function changebackr(){
+    document.getElementById("read").innerHTML= "READ OUR STORY";
+}
+function changebackd(){
+    document.getElementById("download").innerHTML= "DOWNLOAD APP";
+}
+function changebackk(){
+    document.getElementById("know").innerHTML= "KNOW MORE";
+}
+
